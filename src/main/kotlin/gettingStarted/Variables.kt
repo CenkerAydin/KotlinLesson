19580073c76abf7a4b4d1a8847c9a7a5fdd25f90
Cenker Aydın <cenkeraydincse@gmail.com>
2023-06-25 16:13:07 +0300

    Kotlin

---
+++ src/main/kotlin/gettingStarted/Variables.kt
@@ -0,0 +1,22 @@
+package gettingStarted
+
+fun main(){
+    val name= "Cenker"
+    var surName ="Aydın"
+
+    println(name)
+    println(surName)
+
+    surName="k"
+    // name ="c"
+    println(name)
+    println(surName)
+
+    val  age:Int=21
+    print(age)
+    val eyesColor= "Blue"
+    print(eyesColor)
+
+    val skinTone = "White"
+    print(skinTone)
+}
