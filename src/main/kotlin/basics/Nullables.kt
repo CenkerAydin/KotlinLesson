9587a561272667751aa86d963086633264f57520
Cenker Aydın <cenkeraydincse@gmail.com>
2023-07-02 13:19:54 +0300

    Worked Kotlin

--- src/main/kotlin/basics/Nullables.kt
+++ src/main/kotlin/basics/Nullables.kt
@@ -1,5 +1,14 @@
 package basics
 
 fun main() {
+    val name:String? =null
+    val age:Int?=null
 
+    val number =null //Nothing?
+
+    var result:Int?=0
+   // result.plus(324) //Only safe (?.) or non-null asserted (!!.)
+    // calls are allowed on a nullable receiver of type Int?
+    result!!.plus(324)
+    result?.plus(32)
 }
