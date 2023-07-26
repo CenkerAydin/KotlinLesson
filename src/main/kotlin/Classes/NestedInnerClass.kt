29e878702a5b9d3d2a7fee73e2ac56726d8f7d79
Cenker Aydın <cenkeraydincse@gmail.com>
2023-07-26 17:09:21 +0300

    Classes Worked

---
+++ src/main/kotlin/Classes/NestedInnerClass.kt
@@ -0,0 +1,47 @@
+package Classes
+
+
+
+class Outer{
+    private val name="Cenker"
+    val surName="Aydın"
+    fun getAge():Int =21
+    class NestedClass{
+        fun printName(){
+          //  name
+           // surName
+           // getAge()
+        }
+    }
+}
+
+interface OuterInterface{
+
+    class DenemeA
+    interface NestedInterface
+}
+
+class Outer2{
+        private val name="Cenker"
+        val surName="Aydın"
+
+    fun getAge():Int =21
+    inner class InnerClass{
+        fun printName(){
+            name
+            surName
+            getAge()
+        }
+    }
+}
+
+fun main() {
+    val outer=Outer()
+    outer.getAge()
+    outer.surName
+
+    Outer.NestedClass().printName()
+    // Outer().NestedClass().printName
+
+    Outer2().InnerClass().printName()
+}
